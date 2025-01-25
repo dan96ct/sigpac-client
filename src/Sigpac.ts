2bb58d71540bf3a70ed6bfe800757c0e152ccb62
Daniel Cebrián <33624218+dan96ct@users.noreply.github.com>
2025-01-25 16:47:35 +0000

    Solucionado endpoint de consulta y nuevo formateo de codigo

--- src/Sigpac.ts
+++ src/Sigpac.ts
@@ -1,37 +1,46 @@
-import fetch from 'node-fetch';
-import * as globalMercator from 'global-mercator';
-import { transformCoords } from './util/transformCoords';
-import { config } from './config';
-import { ConfigUser, Consulta, Geojson } from './types';
+import fetch from "node-fetch";
+import * as globalMercator from "global-mercator";
+import { transformCoords } from "./util/transformCoords";
+import { config } from "./config";
+import { ConfigUser, Consulta, Geojson } from "./types";
 
-const url = 'https://sigpac.mapama.gob.es';
-
+const url = "https://sigpac.mapama.gob.es";
 
 /**
  * Devuelve un geojson con las datos de la localización
  * @param {String} capa Capa de los datos [  parcela, recinto, declaracion, red_natura, montanera, nitratos, fitosanitarios, pastos_permanentes, e_paisaje_linea, sie, mfe, cp_no_integradas ]
- * @param {Object} coords Objeto con la longitud y latitud [ lng, lat ] 
+ * @param {Object} coords Objeto con la longitud y latitud [ lng, lat ]
  * @param {ConfigUser} configUser Objeto con la configuración del usuario para sobreescribir la original [ transformCoords, projection, proxy ]
  * @returns {Geojson} Objeto con el geojson de las parcelas
  */
-const localizacion = async (capa: string, coords: {lng: number, lat: number}, configUser: ConfigUser): Promise<Geojson> => {
-     const { lng, lat } = coords;
-     const configOverride = {...config, ...configUser};
+const localizacion = async (
+  capa: string,
+  coords: { lng: number; lat: number },
+  configUser: ConfigUser
+): Promise<Geojson> => {
+  const { lng, lat } = coords;
+  const configOverride = { ...config, ...configUser };
 
-     if(!lng || !lat || !capa){
-          throw new Error('Datos incorrectos.');
-     }
-     const tiles = globalMercator.lngLatToTile([Number(lng), Number(lat)], 15);
-     const options = {
-          method: 'GET',
-          headers: {'Content-Type': 'application/json'}
-     }
+  if (!lng || !lat || !capa) {
+    throw new Error("Datos incorrectos.");
+  }
+  const tiles = globalMercator.lngLatToTile([Number(lng), Number(lat)], 15);
+  const options = {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  };
 
-     let response = await fetch((configOverride.proxy ? '' : url) + `/vectorsdg/vector/${capa}@3857/15.${tiles[0]}.${tiles[1]}.geojson`, options)
-     let geojson = await response.json() as Geojson;
-     
-     return (configOverride.transformCoords ? transformCoords(geojson, configOverride.projection) : geojson);
-}
+  let response = await fetch(
+    (configOverride.proxy ? "" : url) +
+      `/vectorsdg/vector/${capa}@3857/15.${tiles[0]}.${tiles[1]}.geojson`,
+    options
+  );
+  let geojson = (await response.json()) as Geojson;
+
+  return configOverride.transformCoords
+    ? transformCoords(geojson, configOverride.projection)
+    : geojson;
+};
 
 /**
  * Devuelve un geojson con los datos
@@ -39,47 +48,69 @@ const localizacion = async (capa: string, coords: {lng: number, lat: number}, co
  * @param {ConfigUser} configUser Objeto con la configuración del usuario para sobreescribir la original [ proxy ]
  * @returns {Geojson} Objeto con los datos de la busqueda
  */
-const buscar = async (data: { [key: string]: number }, configUser: ConfigUser): Promise<Geojson> => {
-     const { comunidad, provincia, municipio, poligono, parcela } = data;
-     const configOverride = {...config, ...configUser};
+const buscar = async (
+  data: { [key: string]: number },
+  configUser: ConfigUser
+): Promise<Geojson> => {
+  const { comunidad, provincia, municipio, poligono, parcela } = data;
+  const configOverride = { ...config, ...configUser };
 
-     const options = {
-          method: 'GET',
-          headers: {'Content-Type': 'application/json'}
-     }
-     if (comunidad) {
-          if (provincia) {
-               if (municipio) {
-                    if (poligono) {
-                         if(parcela){
-                              let response = await fetch((configOverride.proxy ? '' : url) + `/fega/ServiciosVisorSigpac/query/recintos/${provincia}/${municipio}/0/0/${poligono}/${parcela}.geojson`, options);
-                              let geojson = await response.json();
-                              return geojson;
-                         }else{
-                              let response = await fetch((configOverride.proxy ? '' : url) + `/fega/ServiciosVisorSigpac/query/parcelas/${provincia}/${municipio}/0/0/${poligono}.geojson`, options);
-                              let geojson = await response.json();
-                              return geojson;
-                         }
-                    } else {
-                         let response = await fetch((configOverride.proxy ? '' : url) + `/fega/ServiciosVisorSigpac/query/poligonos/${provincia}/${municipio}/0/0.geojson`, options);
-                         let geojson = await response.json();
-                         return geojson;
-                    }
-               } else {
-                    let response = await fetch((configOverride.proxy ? '' : url) + `/fega/ServiciosVisorSigpac/query/municipios/${provincia}.geojson`, options);
-                    let geojson = await response.json();
-                    return geojson;
-               }
+  const options = {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  };
+  if (comunidad) {
+    if (provincia) {
+      if (municipio) {
+        if (poligono) {
+          if (parcela) {
+            let response = await fetch(
+              (configOverride.proxy ? "" : url) +
+                `/fega/ServiciosVisorSigpac/query/recintos/${provincia}/${municipio}/0/0/${poligono}/${parcela}.geojson`,
+              options
+            );
+            let geojson = await response.json();
+            return geojson;
           } else {
-               let response = await fetch((configOverride.proxy ? '' : url) + `/fega/ServiciosVisorSigpac/query/provincias/${comunidad}.geojson`, options);
-               let geojson = await response.json();
-               return geojson;
+            let response = await fetch(
+              (configOverride.proxy ? "" : url) +
+                `/fega/ServiciosVisorSigpac/query/parcelas/${provincia}/${municipio}/0/0/${poligono}.geojson`,
+              options
+            );
+            let geojson = await response.json();
+            return geojson;
           }
-     } else {
-          throw new Error('Se esperaba parametro "comunidad"');
-     }
-}
-
+        } else {
+          let response = await fetch(
+            (configOverride.proxy ? "" : url) +
+              `/fega/ServiciosVisorSigpac/query/poligonos/${provincia}/${municipio}/0/0.geojson`,
+            options
+          );
+          let geojson = await response.json();
+          return geojson;
+        }
+      } else {
+        let response = await fetch(
+          (configOverride.proxy ? "" : url) +
+            `/fega/ServiciosVisorSigpac/query/municipios/${provincia}.geojson`,
+          options
+        );
+        let geojson = await response.json();
+        return geojson;
+      }
+    } else {
+      let response = await fetch(
+        (configOverride.proxy ? "" : url) +
+          `/fega/ServiciosVisorSigpac/query/provincias/${comunidad}.geojson`,
+        options
+      );
+      let geojson = await response.json();
+      return geojson;
+    }
+  } else {
+    throw new Error('Se esperaba parametro "comunidad"');
+  }
+};
 
 /**
  * Devuelve un json con los datos de la capa
@@ -88,66 +119,97 @@ const buscar = async (data: { [key: string]: number }, configUser: ConfigUser): 
  * @param {ConfigUser} configUser Objeto con la configuración del usuario para sobreescribir la original [ proxy ]
  * @returns {Consulta} Objeto con la información de la parcela, recinto o declaracion
  */
-const consulta = async (capa: string, data: { [key: string]: any }, configUser: ConfigUser): Promise<Consulta> => {
-     const configOverride = {...config, ...configUser};
-     enum capas {
-      parcela = 'parcela',
-      recinto = 'recinto',
-      declaracion = 'declaracion'
-     };
+const consulta = async (
+  capa: string,
+  data: { [key: string]: any },
+  configUser: ConfigUser
+): Promise<Consulta> => {
+  const configOverride = { ...config, ...configUser };
+  enum capas {
+    parcela = "parcela",
+    recinto = "recinto",
+    declaracion = "declaracion",
+  }
 
-     if(capa == capas.parcela){
-          const { provincia, agregado, zona, municipio, poligono, parcela } = data;
-          if(provincia == undefined || agregado == undefined || zona == undefined || municipio == undefined || poligono == undefined || parcela == undefined || capa == undefined){
-               throw new Error('Datos incorrectos');
-          }
-          const options = {
-               method: 'GET',
-               headers: {'Content-Type': 'application/json'}
-          }
-          let response = await fetch((configOverride.proxy ? '' : url) + `/fega/ServiciosVisorSigpac/LayerInfo?layer=${capa}&id=${provincia},${municipio},${agregado},${zona},${poligono},${parcela}`, options)
-          let info = await response.json() as Consulta;
-          
-          return info;
-     }
+  if (capa == capas.parcela) {
+    const { provincia, agregado, zona, municipio, poligono, parcela } = data;
+    if (
+      provincia == undefined ||
+      agregado == undefined ||
+      zona == undefined ||
+      municipio == undefined ||
+      poligono == undefined ||
+      parcela == undefined ||
+      capa == undefined
+    ) {
+      throw new Error("Datos incorrectos");
+    }
+    const options = {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    };
+    let response = await fetch(
+      (configOverride.proxy ? "" : url) +
+        `/fega/ServiciosVisorSigpac/LayerInfo?layer=${capa}&id=${provincia},${municipio},${agregado},${zona},${poligono},${parcela}`,
+      options
+    );
+    let info = (await response.json()) as Consulta;
 
+    return info;
+  }
 
-     if(capa == capas.recinto){
-          const { provincia, agregado, zona, municipio, poligono, parcela, recinto } = data;
-          if(provincia == undefined || agregado == undefined || zona == undefined || municipio == undefined || poligono == undefined || parcela == undefined || recinto == undefined || capa == undefined){
-               throw new Error('Datos incorrectos');
-          }
-          const options = {
-               method: 'GET',
-               headers: {'Content-Type': 'application/json'}
-          }
-          let response = await fetch((configOverride.proxy ? '' : url) + `/fega/ServiciosVisorSigpac/LayerInfo?layer=${capa}&id=${provincia},${municipio},${agregado},${zona},${poligono},${parcela},${recinto}`, options)
-          let info = await response.json() as Consulta;
-          
-          return info;
-     }
+  if (capa == capas.recinto) {
+    const { provincia, agregado, zona, municipio, poligono, parcela, recinto } =
+      data;
+    if (
+      provincia == undefined ||
+      agregado == undefined ||
+      zona == undefined ||
+      municipio == undefined ||
+      poligono == undefined ||
+      parcela == undefined ||
+      recinto == undefined ||
+      capa == undefined
+    ) {
+      throw new Error("Datos incorrectos");
+    }
+    const options = {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    };
+    let response = await fetch(
+      (configOverride.proxy ? "" : url) +
+        `/fega/ServiciosVisorSigpac/LayerInfo?layer=${capa}&id=${provincia},${municipio},${agregado},${zona},${poligono},${parcela},${recinto}`,
+      options
+    );
+    let info = (await response.json()) as Consulta;
 
-     if(capa == capas.declaracion){
-          const { lng, lat } = data;
-          if(lng == undefined || lat == undefined){
-               throw new Error('Datos incorrectos');
-          }
-          const options = {
-               method: 'GET',
-               headers: {'Content-Type': 'application/json'}
-          }
-          let response = await fetch((configOverride.proxy ? '' : url) + `/fega/ServiciosVisorSigpac/query/infodeclaracion/${lng}/${lat}.geojson`, options)
-          let info = await response.json() as Consulta;
-          return info;
-     }
+    return info;
+  }
 
-     throw new Error('Nombre de la capa incorrecta');
+  if (capa == capas.declaracion) {
+    const { lng, lat } = data;
+    if (lng == undefined || lat == undefined) {
+      throw new Error("Datos incorrectos");
+    }
+    const options = {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    };
+    let response = await fetch(
+      (configOverride.proxy ? "" : url) +
+        `/fega/ServiciosVisorSigpac/query/infodeclaracion/${lng}/${lat}.geojson`,
+      options
+    );
+    let info = (await response.json()) as Consulta;
+    return info;
+  }
 
-
-}
+  throw new Error("Nombre de la capa incorrecta");
+};
 
 exports.default = {
-     localizacion,
-     buscar,
-     consulta
-}
+  localizacion,
+  buscar,
+  consulta,
+};
--- test/sigpac.test.js
+++ test/sigpac.test.js
@@ -1,111 +1,108 @@
-const chai = require('chai');
+const chai = require("chai");
 
-const Sigpac = require('../dist/index');
+const Sigpac = require("../dist/index");
 
-describe('Suite de pruebas Sigpac', () => {
-     it('Deberia devolver un objeto con los resultados del filtro', (done) => {
-          const data = {
-               comunidad: 7,
-               provincia: 2,
-               municipio: 79,
-               poligono: 37,
-               parcela: 22
-          };
-          
-          Sigpac.buscar(data)
-                  .then((json) => {
-                         if(typeof(json) == 'object'){
-                              done();
-                         }else{
-                              done('Respuesta inesperada de la api');
-                         }
-                   })
-                   .catch((err) => {
-                         done(err);
-                   });
-     });
+describe("Suite de pruebas Sigpac", () => {
+  it("Deberia devolver un objeto con los resultados del filtro", (done) => {
+    const data = {
+      comunidad: 7,
+      provincia: 2,
+      municipio: 79,
+      poligono: 37,
+      parcela: 22,
+    };
 
-     it('Deberia devolver un objeto con un geojson de las coordenadas', (done) => {
-          const data = {
-               lng: -1.8054282, 
-               lat: 38.9598049
-          }
-          Sigpac.localizacion('parcela', data)
-                  .then((json) => {
-                         if(typeof(json) == 'object'){
-                              done();
-                         }else{
-                              done('Respuesta inesperada de la api');
-                         }
-                   })
-                   .catch((err) => {
-                         done(err);
-                   });
-     })
+    Sigpac.buscar(data)
+      .then((json) => {
+        if (typeof json == "object") {
+          done();
+        } else {
+          done("Respuesta inesperada de la api");
+        }
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
 
-     it('Deberia devolver un objeto con con informacion de una parcela', (done) => {
-          const data = {
-               provincia: 2,
-               agregado: 0,
-               zona: 0,
-               municipio: 79,
-               poligono: 37,
-               parcela: 22
-          };
-          Sigpac.consulta('parcela', data)
-               .then((json) => {
-                         
-                         if(typeof(json) == 'object'){
-                              done();
-                         }else{
-                              done('Respuesta inesperada de la api');
-                         }
-               })
-               .catch((err) => {
-                         done(err);
-               });
-     });
+  it("Deberia devolver un objeto con un geojson de las coordenadas", (done) => {
+    const data = {
+      lng: -1.8054282,
+      lat: 38.9598049,
+    };
+    Sigpac.localizacion("parcela", data)
+      .then((json) => {
+        if (typeof json == "object") {
+          done();
+        } else {
+          done("Respuesta inesperada de la api");
+        }
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
 
-     it('Deberia devolver un objeto con con informacion de un recinto', (done) => {
-          const data = {
-               provincia: 2,
-               agregado: 0,
-               zona: 0,
-               municipio: 79,
-               poligono: 37,
-               parcela: 22,
-               recinto: 1
-          };
-          Sigpac.consulta('recinto', data)
-               .then((json) => {
-                         
-                         if(typeof(json) == 'object'){
-                              done();
-                         }else{
-                              done('Respuesta inesperada de la api');
-                         }
-               })
-               .catch((err) => {
-                         done(err);
-               });
-     });
+  it("Deberia devolver un objeto con informacion de una parcela", (done) => {
+    const data = {
+      provincia: 2,
+      agregado: 0,
+      zona: 0,
+      municipio: 79,
+      poligono: 37,
+      parcela: 22,
+    };
+    Sigpac.consulta("parcela", data)
+      .then((json) => {
+        if (typeof json == "object") {
+          done();
+        } else {
+          done("Respuesta inesperada de la api");
+        }
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
 
-     it('Deberia devolver un objeto con con informacion de una linea de declaración', (done) => {
-          const data = {
-               lng: -1.8054282, 
-               lat: 38.9598049
-          };
-          Sigpac.consulta('declaracion', data)
-               .then((json) => {
+  it("Deberia devolver un objeto con informacion de un recinto", (done) => {
+    const data = {
+      provincia: 2,
+      agregado: 0,
+      zona: 0,
+      municipio: 79,
+      poligono: 37,
+      parcela: 22,
+      recinto: 1,
+    };
+    Sigpac.consulta("recinto", data)
+      .then((json) => {
+        if (typeof json == "object") {
+          done();
+        } else {
+          done("Respuesta inesperada de la api");
+        }
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
 
-                         if(typeof(json) == 'object'){
-                              done();
-                         }else{
-                              done('Respuesta inesperada de la api');
-                         }
-               })
-               .catch((err) => {
-                         done(err);
-               });
-     });
+  it("Deberia devolver un objeto con informacion de una linea de declaración", (done) => {
+    const data = {
+      lng: -1.8054282,
+      lat: 38.9598049,
+    };
+    Sigpac.consulta("declaracion", data)
+      .then((json) => {
+        if (typeof json == "object") {
+          done();
+        } else {
+          done("Respuesta inesperada de la api");
+        }
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
 });
